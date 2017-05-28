57027bd8f33ae48f2aba070400fcb45834af2c41
lorenzoturrino <lorenzo.turrino@gmail.com>
2017-05-28 15:41:46 +0100

    integrate aws profile var

--- conf.tf
+++ conf.tf
@@ -1,5 +1,6 @@
-variable "access_key" {}
-variable "secret_key" {}
+variable "profile" {
+  default = "personal"
+}
 variable "region" {
   default = "eu-west-1"
 }
--- terra.tf
+++ terra.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
+  profile = "${var.profile}"
+  region = "${var.region}"
+
 }
 
 resource "aws_instance" "example" {
@@ -9,11 +9,11 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
 }
 
-resource "aws_instance" "another" {
-  ami           = "ami-01ccc867"
-  instance_type = "t2.micro"
-}
-
-resource "aws_eip" "ip" {
-  instance = "${aws_instance.example.id}"
-}
+# resource "aws_instance" "another" {
+#   ami           = "ami-01ccc867"
+#   instance_type = "t2.micro"
+# }
+#
+# resource "aws_eip" "ip" {
+#   instance = "${aws_instance.example.id}"
+# }
